7de34bbd014fef9db2446f7fa42d9d5743e5e54a
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-28 20:53:44 -0400

    cleaned up files and routes, css still not importing properly

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import BaseLayout from './Baselayout';
-import ChildComponent from './Childcomponent';
-import DisplayComponent from './Displaycomponent';
-import Footer from './Footer';
-import Header from './Header';
 import ParentComponent from './Parentcomponent';
-
-<div>
-  <Header />
-  <ChildComponent />
-  <DisplayComponent />
-  <Footer />
-  <ParentComponent />
-</div>
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BaseLayout></BaseLayout>
-        <Header />
+        <BaseLayout />
         <ParentComponent />
-        <Footer />
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="/">Navbar</a>
       </nav>
     );
   }
